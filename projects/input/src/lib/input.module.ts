82d9595261e010f0fa7045c6179b9d00048f2d4b
Alvion work <alvionwork@xaosaki-MacBook-Pro.local>
2020-05-13 11:05:49 +0300

    feat: add lerna, projects and commitizen
    BREAKING CHANGE: add lerna

---
+++ projects/input/src/lib/input.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { InputComponent } from './input.component';
+
+
+
+@NgModule({
+  declarations: [InputComponent],
+  imports: [
+  ],
+  exports: [InputComponent]
+})
+export class InputModule { }
